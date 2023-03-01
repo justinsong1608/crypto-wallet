a2dcfc1ebefb66e488a12a5d8a89dc7e8a11d76d
Justin Song <justinsong1608@gmail.com>
2023-03-01 00:09:36 +0000

    Edited header tab and created a renderCrypto function. #1

---
+++ js/main.js
@@ -0,0 +1,50 @@
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://api.coincap.io/v2/assets');
+xhr.responseType = 'json';
+xhr.addEventListener('load', function () {
+
+});
+xhr.send();
+
+function renderCrypto(crypto) {
+  var $tr = document.createElement('tr');
+
+  var $tdOne = document.createElement('td');
+  $tdOne.textContent = crypto.rank;
+  $tr.appendChild($tdOne);
+
+  var $tdTwo = document.createElement('td');
+  $tdTwo.textContent = crypto.name + '(' + crypto.symbol + ')';
+  $tr.appendChild($tdTwo);
+
+  var $tdThree = document.createElement('td');
+  $tdThree.textContent = parseFloat(crypto.changePercent24Hr).toFixed(2) + '%';
+  $tr.appendChild($tdThree);
+
+  var $tdFour = document.createElement('td');
+  $tdFour.textContent = parseFloat(crypto.priceUsd).toFixed(2);
+  $tr.appendChild($tdFour);
+
+  var $tdFive = document.createElement('td');
+  $tdFive.textContent = parseFloat(crypto.marketCapUsd).toFixed(0);
+  $tr.appendChild($tdFive);
+
+  var $tdSix = document.createElement('td');
+  $tdSix.textContent = parseFloat(crypto.volumeUsd24Hr).toFixed(0);
+  $tr.appendChild($tdSix);
+
+  var $tdSeven = document.createElement('td');
+  $tdSeven.textContent = parseFloat(crypto.maxSupply).toFixed(0);
+  $tr.appendChild($tdSeven);
+
+  var $tdEight = document.createElement('td');
+  $tr.appendChild($tdEight);
+
+  var $iTag = document.createElement('i');
+  $iTag.setAttribute('class', 'fa-solid fa-plus');
+  $tdEight.appendChild($iTag);
+
+  return $tr;
+}
+
+renderCrypto(crypto);
